be2eb5480cf18ff060a2ed463d5fbdedbdfc2b63
Spook <spook.devv@gmail.com>
2024-03-31 11:38:54 -0400

    Naviggating with the <Link> component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 function Index() {
   return (
     <div>
       <h1>The Home page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 }
