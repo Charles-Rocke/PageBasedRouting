500a04dad8252c53a4353c15317770da9d745128
Spook <spook.devv@gmail.com>
2024-03-31 11:44:22 -0400

    Navigating to dynamically created routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
 function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maxi" },
+    { id: "Tony", name: "Tonysan" },
+  ];
   return (
     <div>
       <h1>The Clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            {" "}
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
