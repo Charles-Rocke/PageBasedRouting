fc0527967cf6d54f4ccc109f865c7b16cc4b675a
Spook <spook.devv@gmail.com>
2024-03-31 11:52:33 -0400

    Navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,22 @@ import { useRouter } from "next/router";
 function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/project-a"); option one
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "A",
+      },
+    });
+  }
   return (
     <div>
       <h1>The projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 }
